6eed50c4627c40c28fdbae0ede8d1e615a791efe
Michel Albonico <bsd.albonico@gmail.com>
2021-04-16 15:21:15 -0300

    Update connect.php

--- web/includes/connect.php
+++ web/includes/connect.php
@@ -2,13 +2,12 @@
 
      $servidor = "localhost";
      $porta = 5432;
-     $bancoDeDados = "nome_do_banco";
+     $bancoDeDados = "atlas_v7";
      $usuario = "postgres";
-     $senha = "senha_do_banco";
+     $senha = "postgres123";
 
 	$con_string = "host=$servidor port=$porta dbname=$bancoDeDados user=$usuario password=$senha";
 
 	if(!$dbcon = pg_connect($con_string)) die ("Erro ao conectar ao banco<br>".pg_last_error($dbcon));
-
 
 ?>
